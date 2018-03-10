4160f98136430bbc533b7a3c405358efdd43d13d
bmroczek <bmroczek@pl.sii.eu>
2018-03-10 19:31:35 +0100

    Coment after code review

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Calculator {
 
+
+    //Pomyśl czy faktycznie wszystkie zmienne są ci potrzebne globalnie.
     private double grossSalary;
     private double socialContributions;
     private double baseOfIncomeTax;
@@ -44,15 +46,20 @@ public class Calculator {
         return grossSalary;
     }
 
+    //Ta metoda może być typu void
     private String askAboutWorkPlace(){
         boolean repeat = true;
         List<String> possibleAnswer = getPossibleAnswer();
         do {
             System.out.println("Czy wykonujesz prace poza miejscem zamieszkania? (Y/N)");
             Scanner input2 = new Scanner(System.in);
+            //ponieważ tutaj przypisujesz już wartość zmiennej globalnej
+            //pamimętaj że masz zmienne globalne i lokalne. Możesz to wykorzystać tutaj i przerobić
+            //tą metodę tak aby faktynie musiała coś zwracać.
             answer = input2.nextLine();
             repeat = !possibleAnswer.contains(answer);
         }while(repeat);
+        //Tutaj zwracasz zmienną answer. Do której już przypisałeś wartość.
         return answer;
     }
     private List<String> getPossibleAnswer() {
@@ -68,9 +75,14 @@ public class Calculator {
     }
 
     private boolean checkThatFromAnotherCity(){
+        //zwróć uwagę na to ile masz poprawnych odpowiedzi w metodzie getPossibleAnswer()
+        //a ile masz tutaj.
         return answer.equals("Y");
     }
 
+    //zwróć uwagę że socialContributions masz dostępne jako zmienną globalną.
+    //Więc nie bardzo jest sens przekazywać tutaj ją jakos argument.
+    //albo zrób tak żeby nie była dostępna jako globalna albo zrób metodę jako bez argumentową
     private double calculateBaseOfIncomeTax(double socialContributions){
         boolean fromAnotherCity = checkThatFromAnotherCity();
         if (fromAnotherCity) {
@@ -82,6 +94,8 @@ public class Calculator {
     private double calculateHealthInsurance(){
         baseOfHealthInsurance = grossSalary - socialContributions;
         healthInsurance = baseOfHealthInsurance * 0.09;
+        //Tutaj robisz to samo co wcześniej. Masz zmienną dostępną globalnie, przypisujesz jej wartość a następnie zwracasz.
+        //Pamiętaj że jeżeli chcesz zwracać jakąś zmienną to ma to sens tylko jeżeli jest ona lokalna. Chyba że metoda jest getterem.
         return healthInsurance;
     }
 
@@ -91,21 +105,25 @@ public class Calculator {
            incomeTax = baseOfIncomeTax * 0.18;
         }
         incomeTax = baseOfIncomeTax * 0.32;
+        //Znowu zasięg zmiennej. Czy ona musi być globalna czy nie?
         return incomeTax - 46.33;
     }
 
     private double calculateHealthCareContribution(){
         healthCareContribution = baseOfHealthInsurance * 0.0775;
+        //To samo.
         return healthCareContribution;
     }
 
     private double calculateIncomeTaxToBeCollected(){
         incomeTaxToBeCollected = incomeTax - healthCareContribution;
+        //Znowu
         return incomeTaxToBeCollected;
     }
 
     private double calculateNetIncome(){
         netIncome = grossSalary - (socialContributions - healthInsurance - incomeTaxToBeCollected);
+        // I raz jeszcze
         return netIncome;
     }
 
